1c423324a18cf6ac84a5eb2ceb5faaa6543cf342
SOMIL JAIN <42209500+somiljain7@users.noreply.github.com>
2019-10-08 18:34:13 +0530

    ML BASICS TENSORFLOW

--- 2.py
+++ 2.py
@@ -1,16 +1,18 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf 
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense
 
-import os
+class SimpleNeuralNetwork(Model):
+  def __init__(self):
+    super(SimpleNeuralNetwork , self).__init__()
+    self.layer1 = Dense(2,activation='relu')
+    self.layer2 = Dense(3,activation='relu')
+    self.outputLayer = Dense(1,activation='softmax')
+  def call(self, x):
+    x = self.layer1(x)
+    x = self.layer2(x)
+    return self.outputLayer(x)
 
-import tensorflow as tf
-from tensorflow import keras
-
-
-import numpy as np
-
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-zip_file = tf.keras.utils.get_file(origin="https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip",
-                                   fname="cats_and_dogs_filtered.zip", extract=True)
-base_dir, _ = os.path.splitext(zip_file)
-
+Model = SimpleNeuralNetwork()
+     
+ 
